896fdb1723f9790d3963d600f8320684edb40059
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-06-02 18:28:35 -0400

    feat: se implementa y enruta el registro de usuarios

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 def home(request):
     return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
